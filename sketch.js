db53936961aef1f0375eefaefe57448e27bdaeb5
sacchetantonio <antonio.sacchet@mail.polimi.it>
2021-09-28 21:48:31 +0200

    provaConsegna

--- sketch.js
+++ sketch.js
@@ -5,9 +5,442 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
+  angleMode(DEGREES);
+  background("#262624");
 }
 
 function draw() {
   // put drawing code here
-  background("green");
+  noStroke();
+
+  let x = windowWidth / 2;
+  let y = windowHeight - frameCount;
+
+  let r = 50 - frameCount / 15;
+
+  let n = abs(cos(frameCount / ((50 * 15) / 90)));
+
+  let myColor1 = lerpColor(color("#262624"), color("#e6d500"), n);
+  let myColor2 = lerpColor(color("#262624"), color("#e6332a"), n);
+  let myColor3 = lerpColor(color("#262624"), color("#f39200"), n);
+  let myColor4 = lerpColor(color("#262624"), color("#be1622"), n);
+
+  if (frameCount === 50 * 15) {
+    //scale(1, 0.5, 1);
+    //translate(0, -windowHeight / 5);
+
+    frameCount = 0;
+  }
+
+  push();
+
+  translate(50 * cos(frameCount), windowHeight / 7.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 16 + 50 * sin(frameCount), windowHeight / 10);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 8 + 50 * cos(frameCount), windowHeight / 50);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+
+  translate(-windowWidth / 6 + 50 * cos(frameCount), windowHeight / 10);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+
+  translate(-windowWidth / 4 + 50 * sin(frameCount), windowHeight / 6);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+  push();
+  translate(-windowWidth / 2.5 + 50 * cos(frameCount), windowHeight / 3);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 16 + 50 * sin(frameCount), windowHeight / 3);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+  //x = windowWidth / 2;
+  // y = windowHeight - frameCount;
+
+  push();
+
+  translate(windowWidth / 8 + 50 * cos(frameCount), windowHeight / 10);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 6 + 50 * sin(frameCount), windowHeight / 25);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 4 + 50 * cos(frameCount), windowHeight / 10);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+
+  translate(windowWidth / 2.5 + 50 * cos(frameCount), windowHeight / 7);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 3 + 50 * sin(frameCount), windowHeight / 4);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 3 + 50 * cos(frameCount), windowHeight / 3);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 5 + 50 * sin(frameCount), windowHeight / 5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 5 + 50 * sin(frameCount), windowHeight / 4);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 5 + 50 * sin(frameCount), windowHeight / 2);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 4 + 50 * sin(frameCount), windowHeight / 1.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 3 + 50 * cos(frameCount), windowHeight / 2);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 2.5 + 50 * sin(frameCount), windowHeight / 2.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 6 + 50 * sin(frameCount), windowHeight / 2);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 8 + 50 * cos(frameCount), windowHeight / 1.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(-windowWidth / 16 + 50 * cos(frameCount), windowHeight / 2);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 16 + 50 * sin(frameCount), windowHeight / 2.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 8 + 50 * sin(frameCount), windowHeight / 1.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 6 + 50 * cos(frameCount), windowHeight / 2);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 5 + 50 * sin(frameCount), windowHeight / 2.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 4 + 50 * cos(frameCount), windowHeight / 1.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 3 + 50 * cos(frameCount), windowHeight / 2);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
+
+  push();
+  translate(windowWidth / 2.5 + 50 * sin(frameCount), windowHeight / 1.5);
+
+  fill(myColor1);
+  arc(x, y, r, r, frameCount, frameCount + 90, PIE);
+  fill(myColor2);
+  arc(x, y, r, r, frameCount + 90, frameCount + 180, PIE);
+  fill(myColor3);
+  arc(x, y, r, r, frameCount + 180, frameCount + 270, PIE);
+  fill(myColor4);
+  arc(x, y, r, r, frameCount + 270, frameCount + 360, PIE);
+
+  pop();
 }
